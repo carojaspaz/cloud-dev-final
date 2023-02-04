4e643cb228cd0115c67a7e49b63f77646f1f5565
Carlos Andrés Rojas Paz <carojaspaz@gmail.com>
2023-02-04 00:28:49 -0500

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import imageRickMorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Charactres from "./components/Characters";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async() => {
+    await fetch('https://rickandmortyapi.com/api/character/')
+    .then(response => response.json())
+    .then(data => {
+      setCharacters(data.results);
+    });
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Charactres characters={characters} setCharacters={setCharacters}/>
+        ):(
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
